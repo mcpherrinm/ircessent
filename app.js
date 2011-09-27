19dedb9b4ae21e1961612574dd459cb018d4f42f
Matthew McPherrin <matthew@mcpherrin.ca>
2011-09-27 04:54:00 -0400

    Started basic client & app

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = require('http').createServer(handler)
+ , io = require('socket.io').listen(app)
+ , fs = require('fs')
+
+app.listen(9000);
+
+function handler(req, res) {
+ fs.readFile(__dirname + "/chat.html",
+  function(err, data) {
+   if(err) {
+    res.writeHead(500);
+    return res.end('Error');
+   }
+   res.writeHead(200);
+   res.end(data);
+  });
+}
+
+io.sockets.on('connection', function(socket) {
+ socket.emit('statuschange', { status: 'START' });
+ socket.on('statusack', function(data) {
+  console.log(data);
+ });
+});
